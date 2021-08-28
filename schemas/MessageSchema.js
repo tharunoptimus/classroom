9b0d8842ad574c3ff948f04ee3e0661d694d8525
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-08-29 02:01:11 +0530

    Changed Message Schema Removed the isOwner Attribute

--- schemas/MessageSchema.js
+++ schemas/MessageSchema.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const MessagesSchema = new Schema({
     classId: { type: Schema.Types.ObjectId, ref: "Class" },
-    
     sender: { type: String, required: true, ref: "User" },
-    isOwner: { type: Boolean, required: true },
-    message: { type: String, trim: true }
+    content: { type: String, trim: true }
 
 }, { timestamps: true });
 
